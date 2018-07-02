13fa6faec9eb9a3bc97254d0080e2abadedc8ec6
eliasdabbas <eliasdabbas@gmail.com>
2018-07-02 06:17:06 +0400

    Add init imports

--- advertest/__init__.py
+++ advertest/__init__.py
@@ -5,3 +5,10 @@
 __author__ = """Elias Dabbas"""
 __email__ = 'eliasdabbas@gmail.com'
 __version__ = '0.1.0'
+
+from advertest.ad_create import ad_create
+from advertest.ad_from_string import ad_from_string
+from advertest.kw_generate import *
+from advertest.stopwords import stopwords
+from advertest.url_builders import url_utm_ga
+from advertest.word_frequency import word_frequency
